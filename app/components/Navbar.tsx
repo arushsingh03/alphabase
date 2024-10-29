797526145efdfccb048a2329858f4a36203567e8
Arush Singh <sarushmad@gmail.com>
2024-10-29 14:20:16 +0530

    initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,52 @@
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = async () => {
+  const session = await auth();
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="" className="flex items-center space-x-2 ">
+          <Image src="/logo.png" alt="logo" width={55} height={30} />
+          <p className="text-2xl font-extrabold font-serif">
+            Alpha <span className="text-pink-500 -ml-1 ">Base</span>
+          </p>
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
